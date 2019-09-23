63de2b517318e5e1b6137a1bd421601a9cf828f5
JiaXinWang61 <584937529@qq.com>
2019-09-23 19:08:31 +0800

    builder模式实战

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -3,4 +3,33 @@ package com.github.hcsp.encapsulation;
 public class UserBuilder {
     // 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+
+    public UserBuilder FirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public UserBuilder LastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public UserBuilder PhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UserBuilder Address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public User build() {
+        return new User(firstName, lastName, phoneNumber, address);
+    }
+
 }
