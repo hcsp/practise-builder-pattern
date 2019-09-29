38460d7ee6c9edc412e68622d671b8d4de6684f6
张新望 <zxw1992513@gmail.com>
2019-09-29 23:09:23 +0800

    Update UserBuilder.java (#67)
    Update UserBuilder.java (#67)

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -1,6 +1,39 @@
 package com.github.hcsp.encapsulation;
 
-public class UserBuilder {
-    // 请在这里使用builder模式建造User对象
-    // 所需的接口请参阅UserBuilderTest测试类
+public final class UserBuilder {
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private String address;
+
+  UserBuilder() {
+  }
+
+  public static UserBuilder anUser() {
+    return new UserBuilder();
+  }
+
+  public UserBuilder firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public UserBuilder lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public UserBuilder phoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    return this;
+  }
+
+  public UserBuilder address(String address) {
+    this.address = address;
+    return this;
+  }
+
+  public User build() {
+    return new User(firstName, lastName, phoneNumber, address);
+  }
 }
