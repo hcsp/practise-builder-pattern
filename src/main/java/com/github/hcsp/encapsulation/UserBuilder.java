a56369c36a709ba939d8d892e33c79032fa57278
Maricaya <915270549@qq.com>
2019-12-06 21:20:53 +0800

    feat: builder-pattern

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -3,4 +3,35 @@ package com.github.hcsp.encapsulation;
 public class UserBuilder {
     // 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String address;
+
+    public UserBuilder getFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public UserBuilder getLastName(String lastName) {
+        this.firstName = lastName;
+        return this;
+    }
+
+    public UserBuilder getPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UserBuilder getAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public User Builder() {
+        return new User(firstName, lastName, phoneNumber, address);
+    }
 }
