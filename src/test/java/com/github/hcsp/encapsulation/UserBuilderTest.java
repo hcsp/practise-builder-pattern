b5d592b83cc72f41399c204110776328ff38c1d7
yaohengfeng <1065435454@qq.com>
2019-12-26 11:08:11 +0800

    修改bug

---
+++ src/test/java/com/github/hcsp/encapsulation/UserBuilderTest.java
@@ -0,0 +1,21 @@
+package com.github.hcsp.encapsulation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserBuilderTest {
+    @Test
+    public void canUseBuilderToCreateObjects() {
+        User user =
+                new UserBuilder()
+                        .firstName("德华")
+                        .LastName("刘")
+                        .phoneNumber("我也不知道")
+                        .address("可能在地球上吧")
+                        .build();
+        Assertions.assertEquals("德华", user.getFirstName());
+        Assertions.assertEquals("刘", user.getLastName());
+        Assertions.assertEquals("我也不知道", user.getPhoneNumber());
+        Assertions.assertEquals("可能在地球上吧", user.getAddress());
+    }
+}
