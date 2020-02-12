900eaeb5a3d2cf8e3d9bc834305e19cd2d21fa64
maxiaoda <631677979@qq.com>
2020-02-12 13:03:25 +0800

    Java面向对象初步练习：builder模式实战

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -1,29 +1,29 @@
 package com.github.hcsp.encapsulation;
 
 public class UserBuilder {
-    // 请在这里使用builder模式建造User对象
-    // 所需的接口请参阅UserBuilderTest测试类
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String address;
 
-    public UserBuilder withFirstName(String firstName) {
+    // 请在这里使用builder模式建造User对象
+    // 所需的接口请参阅UserBuilderTest测试类
+    public UserBuilder firstName(String firstName) {
         this.firstName = firstName;
         return this;
     }
 
-    public UserBuilder withLastName(String lastName) {
+    public UserBuilder lastName(String lastName) {
         this.lastName = lastName;
         return this;
     }
 
-    public UserBuilder withPhoneNumber(String phoneNumber) {
+    public UserBuilder phoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
         return this;
     }
 
-    public UserBuilder withAddress(String address) {
+    public UserBuilder address(String address) {
         this.address = address;
         return this;
     }
