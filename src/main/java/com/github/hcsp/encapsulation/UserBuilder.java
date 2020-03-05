d0bafa42e7f0f49d29e17106f73769e3719bb8ee
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-05 11:28:21 +0800

    builder模式实战 (#148)
    builder模式实战 (#148)

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -3,4 +3,39 @@ package com.github.hcsp.encapsulation;
 public class UserBuilder {
     // 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
+    private String firstname;
+    private String lastname;
+    private String phoneNumber;
+    private String address;
+
+    public UserBuilder() {
+    }
+
+//    public static UserBuilder aUser(){
+//        return new UserBuilder();
+//    }
+
+    public UserBuilder firstName(String firstName) {
+        this.firstname = firstName;
+        return this;
+    }
+
+    public UserBuilder lastName(String lastName) {
+        this.lastname = lastName;
+        return this;
+    }
+
+    public UserBuilder phoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UserBuilder address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public User build() {
+        return new User(firstname, lastname, phoneNumber, address);
+    }
 }
