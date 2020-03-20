f706e437851226e1185bec4f2b71867e54bbe299
chenz <czj2140811@gmail.com>
2020-03-20 12:59:21 -0700

    patten

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -8,33 +8,31 @@ public class UserBuilder {
     private String phoneNumber;
     private String address;
 
-    public String getFirstName(){
-        return firstName;
+    public UserBuilder(){
+
     }
+
     public UserBuilder firstName(String firstName){
         this.firstName = firstName;
         return this;
     }
-    public String getLastName(){
-        return lastName;
-    }
+
     public UserBuilder lastName(String lastName){
         this.lastName = lastName;
         return this;
     }
-    public String getPhoneNumber(){
-        return phoneNumber;
-    }
+
     public UserBuilder phoneNumber(String phoneNumber){
         this.phoneNumber =  phoneNumber;
         return this;
     }
-    public String getAddress(){
-        return address;
-    }
+
     public UserBuilder address(String address){
         this.address = address;
         return this;
+    }
+    public User bulid(){
+        return new User (firstName, lastName, phoneNumber, address);
     }
 
 }
