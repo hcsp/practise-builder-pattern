de5c0a02d72432bf90025b68addf465288e72b59
strivejojo <1603427597@qq.com>
2020-04-09 20:34:04 +0800

    Java面向对象初步练习：builder模式实战

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.hcsp.encapsulation;
-
-public class Main {
-    public static void main(String[] args) {
-        User user = new UserBuilder().firstName("张").lastName("三").build();
-        System.out.println(user.getFirstName());
-        System.out.println(user.getLastName());
-    }
-}
