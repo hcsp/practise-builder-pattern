b2d1a314865af3fb7ced4b4622733523f885a41b
Wong <19876688427>
2020-04-30 11:45:32 +0800

    不知道会不会写错

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -1,6 +1,40 @@
 package com.github.hcsp.encapsulation;
 
-public class UserBuilder {
-    // 请在这里使用builder模式建造User对象
-    // 所需的接口请参阅UserBuilderTest测试类
+public final class UserBuilder {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+
+    private UserBuilder() {
+    }
+
+    public static UserBuilder anUser() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public UserBuilder withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+
+    public UserBuilder withPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UserBuilder withAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public User build() {
+        return new User(firstName, lastName, phoneNumber, address);
+    }
 }
