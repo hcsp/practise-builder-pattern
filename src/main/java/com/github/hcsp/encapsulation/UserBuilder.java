450d8e7f8bf83b5464ad3b6fa87fe7c72983ae24
znzaznz <39003442+znzaznz@users.noreply.github.com>
2020-10-30 17:07:30 +0800

    完成builder的写法 (#222)
    完成builder的写法 (#222)

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -3,4 +3,33 @@ package com.github.hcsp.encapsulation;
 public class UserBuilder {
     // 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+
+    public static UserBuilder anUser() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder firstName(String name){
+        this.firstName = name;
+        return this;
+    }
+    public UserBuilder lastName(String value){
+        this.lastName = value;
+        return this;
+    }
+    public UserBuilder phoneNumber(String value){
+        this.phoneNumber = value;
+        return this;
+    }
+    public UserBuilder address(String value){
+        this.address = value;
+        return this;
+    }
+
+    public User build(){
+        return new User(firstName, lastName, phoneNumber, address);
+    }
 }
