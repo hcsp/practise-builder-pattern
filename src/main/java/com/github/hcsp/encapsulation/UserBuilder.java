5c333f5efe6fd7c2bcf1e930dbeb48bd1276f1b4
Y.L <2180531663@qq.com>
2022-03-21 16:52:13 +0800

    user对象

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -3,4 +3,49 @@ package com.github.hcsp.encapsulation;
 public class UserBuilder {
     // 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
+    private  String firstName;
+    private String lastName;
+    private  String phoneNumber;
+    private String address;
+    private UserBuilder() {
+    }
+
+    public UserBuilder firstName(String firstName){
+        this.firstName = firstName;
+        return this;
+    }
+
+    public UserBuilder lastName(String lastName){
+        this.lastName = lastName;
+        return this;
+    }
+    public UserBuilder phoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+    public UserBuilder address(String address){
+        this.address = address;
+        return this;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public User build() {
+        return new User(this.getFirstName(),this.getLastName(),this.getPhoneNumber(),this.getAddress());
+    }
+
 }
