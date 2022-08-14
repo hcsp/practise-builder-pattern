35774289af49c0b6a5c5621c46be23735fe0baf4
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2022-08-14 22:09:14 +0800

    update home work (#292)
    update home work (#292)

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -1,6 +1,38 @@
 package com.github.hcsp.encapsulation;
 
 public class UserBuilder {
-    // 请在这里使用builder模式建造User对象
-    // 所需的接口请参阅UserBuilderTest测试类
+    /** 用户的名 */
+    private String builderFirstName;
+
+    /** 用户的姓 */
+    private String builderLastName;
+
+    /** 用户的电话 */
+    private String builderPhoneNumber;
+
+    /** 用户的地址 */
+    private String builderAddress;
+
+    public UserBuilder firstName(String firstName){
+        this.builderFirstName = firstName;
+        return this;
+    }
+
+    public UserBuilder lastName(String lastName){
+        this.builderLastName = lastName;
+        return this;
+    }
+    public UserBuilder phoneNumber(String phoneNumber){
+        this.builderPhoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UserBuilder address(String address){
+        this.builderAddress = address;
+        return this;
+    }
+    public User build(){
+        return new User(this.builderFirstName, this.builderLastName,this.builderPhoneNumber, this.builderAddress);
+    }
+
 }
