0c18701b7fce6578b0fef890ea9fa1625fdfe284
EliteFan <fan552426811@outlook.com>
2022-08-21 09:10:22 +0800

    builder

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++
@@ -1,8 +0,0 @@
-package com.github.hcsp.encapsulation;
-
-public class Main {
-    public static void main(String[] args) {
-        User user = UserBuilder.anUser().firstName("fpc").build();
-        System.out.println(user.getLastName());
-    }
-}
