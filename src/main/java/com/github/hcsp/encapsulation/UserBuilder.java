9aaa47c2e12ed2eccf0d20f95331a2462b65c5fa
spearhead0428 <hastur1016@gmail.com>
2023-05-30 00:01:00 +0800

    12312321

--- src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
+++ src/main/java/com/github/hcsp/encapsulation/UserBuilder.java
@@ -1,6 +1,35 @@
 package com.github.hcsp.encapsulation;
 
 public class UserBuilder {
-    // 请在这里使用builder模式建造User对象
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String address;
+
+	public UserBuilder firstName(String test) {
+		this.firstName = test;
+		return this;
+	}
+
+	public UserBuilder lastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public UserBuilder phoneNumber(String PhoneNumber) {
+		this.phoneNumber= PhoneNumber;
+		return this;
+
+	}
+
+	public UserBuilder address(String address) {
+		this.address = address;
+		return this;
+	}
+
+	public User build(){
+		return new User(firstName,lastName,phoneNumber,address);
+	}
+	// 请在这里使用builder模式建造User对象
     // 所需的接口请参阅UserBuilderTest测试类
 }
