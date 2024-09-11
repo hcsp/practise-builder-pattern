260f5f53a46f7ac5fbd870cc5c8fc059175485e4
GJC <599953528@qq.com>
2024-09-11 14:48:55 +0800

    builder

--- src/test/java/com/github/hcsp/encapsulation/UserBuilderTest.java
+++ src/test/java/com/github/hcsp/encapsulation/UserBuilderTest.java
@@ -7,11 +7,11 @@ public class UserBuilderTest {
     @Test
     public void canUseBuilderToCreateObjects() {
         User user =
-                UserBuilder.anUser()
-                        .withFirstName("德华")
-                        .withAddress("刘")
-                        .withPhoneNumber("我也不知道")
-                        .withAddress("可能在地球上吧")
+                new UserBuilder()
+                        .firstName("德华")
+                        .lastName("刘")
+                        .phoneNumber("我也不知道")
+                        .address("可能在地球上吧")
                         .build();
         Assertions.assertEquals("德华", user.getFirstName());
         Assertions.assertEquals("刘", user.getLastName());
